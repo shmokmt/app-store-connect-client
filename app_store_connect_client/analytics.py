dbdcb9654368036433c534493bd4eb8bf007dd72
Shoma Okamoto <shoma.develop@gmail.com>
2020-04-23 14:35:18 +0900

    Fmt.

--- app_store_connect_client/analytics.py
+++ app_store_connect_client/analytics.py
@@ -2,6 +2,8 @@ import requests
 from urllib.parse import urlparse
 import json
 from . import query
+
+
 class Client(object):
     def __init__(self, username, password, is_2fa_auth=False):
         self.is_2fa_auth = is_2fa_auth
@@ -14,78 +16,92 @@ class Client(object):
             "apple_widget_key": "e0b80c3bf78523bfe80974d320935bfa30add02e1bff88ec2166c6bd5a706c42",
         }
         self._headers = {
-            'Content-Type': 'application/json;charset=UTF-8',
-            'Accept': 'application/json, text/plain, */*',
-            'Origin': 'https://analytics.itunes.apple.com',
-            'X-Requested-By': 'analytics.itunes.apple.com',
-            'Referer': 'https://analytics.itunes.apple.com/',
+            "Content-Type": "application/json;charset=UTF-8",
+            "Accept": "application/json, text/plain, */*",
+            "Origin": "https://analytics.itunes.apple.com",
+            "X-Requested-By": "analytics.itunes.apple.com",
+            "Referer": "https://analytics.itunes.apple.com/",
         }
 
         self._login(username, password)
 
-
     def _login(self, username, password):
-        payload = {
-            'accountName': username, 'password': password, 'rememberMe': False
-        }
+        payload = {"accountName": username, "password": password, "rememberMe": False}
         headers = {
-            'Content-Type': 'application/json',
-            'X-Apple-Widget-Key': self._options["apple_widget_key"]
+            "Content-Type": "application/json",
+            "X-Apple-Widget-Key": self._options["apple_widget_key"],
         }
-        r = self._session.post(self._options["login_url"] + "/signin", json=payload, headers=headers)
-        
+        r = self._session.post(
+            self._options["login_url"] + "/signin", json=payload, headers=headers
+        )
+
         headers = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'scnt': r.headers['scnt'],
-            'X-Apple-ID-Session-Id': r.headers['x-apple-id-session-id'],
-            'X-Requested-With': 'XMLHttpRequest',
-            'X-Apple-Domain-Id': '3',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-Mode': 'cors',
-        };
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "scnt": r.headers["scnt"],
+            "X-Apple-ID-Session-Id": r.headers["x-apple-id-session-id"],
+            "X-Requested-With": "XMLHttpRequest",
+            "X-Apple-Domain-Id": "3",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "cors",
+        }
 
         if self.is_2fa_auth:
             print("Enter the 2FA code:")
             two_factor_auth_code = input()
-            self._session.post(self._options["login_url"] + "/verify/trusteddevice/securitycode", headers=headers, data={'securityCode': {'code': two_factor_auth_code}})
-        r = self._session.post(self._options["login_url"] + "/2sv/trust", headers=headers)
-        cookies = r.headers['set-cookie']
+            self._session.post(
+                self._options["login_url"] + "/verify/trusteddevice/securitycode",
+                headers=headers,
+                data={"securityCode": {"code": two_factor_auth_code}},
+            )
+        r = self._session.post(
+            self._options["login_url"] + "/2sv/trust", headers=headers
+        )
+        cookies = r.headers["set-cookie"]
         if cookies is None or len(cookies) == 0:
-            raise Exception("There was a problem with loading the login page cookies. Check login credentials.")
+            raise Exception(
+                "There was a problem with loading the login page cookies. Check login credentials."
+            )
 
         self._session.get(self._options["base_url"] + "/session", allow_redirects=False)
-        if 'myacinfo' not in self._session.cookies.get_dict().keys():
-            raise Exception("There was a problem with loading the login page cookies. Check login credentials.")
+        if "myacinfo" not in self._session.cookies.get_dict().keys():
+            raise Exception(
+                "There was a problem with loading the login page cookies. Check login credentials."
+            )
 
-        if 'itctx' not in self._session.cookies.get_dict().keys():
-            raise Exception("No itCtx cookie :( Apple probably changed the login process")
+        if "itctx" not in self._session.cookies.get_dict().keys():
+            raise Exception(
+                "No itCtx cookie :( Apple probably changed the login process"
+            )
 
     def _get_api_url(self, url):
         res = self.session.get(url, headers=self._headers, timeout=500)
 
-
     def get_apps(self):
-        url = self.options['settings_url'] + '/app-info/all'
+        url = self.options["settings_url"] + "/app-info/all"
         self._get_api_url(url)
-
 
     def get_settings(self):
-        url = self.options['settings_url'] + '/settings/all'
+        url = self.options["settings_url"] + "/settings/all"
         self._get_api_url(url)
-
 
     def change_provider(self, provider_id):
         # TODO: check the workking currect.
-        data = {'provider': {'providerId': provider_id}}
-        self._session.post(url=self._options["base_url"]) 
-
+        data = {"provider": {"providerId": provider_id}}
+        self._session.post(url=self._options["base_url"])
 
     def execute(self, query=None):
         request_body = query.assemble_body()
-        res = self._session.post(query.analytics_url, headers=self._headers, timeout=300000, json=request_body)
+        res = self._session.post(
+            query.analytics_url,
+            headers=self._headers,
+            timeout=300000,
+            json=request_body,
+        )
         if res.status_code == 401:
-            raise Exception("This request requires authentication. Please check your username and password.")
+            raise Exception(
+                "This request requires authentication. Please check your username and password."
+            )
         if res.status_code == 400:
             raise Exception("400 Bad Request. Please check your config.")
         return res.json()
--- app_store_connect_client/query.py
+++ app_store_connect_client/query.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
 from urllib.parse import urlparse
-from . import enum
+from .dataclass import 
+
 class Query(object):
     def __init__(self, app_id):
         self.app_id = app_id
@@ -11,32 +12,37 @@ class Query(object):
             "frequency": "DAY",
             "dimensionFilters": [],
         }
-        self._api_url = 'https://analytics.itunes.apple.com/analytics/api/v1'
+        self._api_url = "https://analytics.itunes.apple.com/analytics/api/v1"
         self._end_point = None
         self._time = None
-    
+
     @property
     def analytics_url(self):
         return urlparse(self._api_url + self._end_point).geturl()
-    
-    def metrics(self, config):
-        self._end_point = "/data/time-series"
-        for key in ["limit", "dimension"]:
+
+    def _clean_config(self, keys):
+        for key in keys:
             if config.get(key):
                 del self.config[key]
+
+    def metrics(self, config):
+        self._end_point = "/data/time-series"
+        self._clean_config(["limit", "dimension"])
         if not self.config.get("group"):
             self.config["group"] = None
         if not self.config.get("dimensionFilters"):
             self.config["dimensionFilters"] = []
         if not self.config.get("measures"):
-            self.config["measures"] = [enum.Measures.installs.value, enum.Measures.crashes.value]
+            self.config["measures"] = [
+                .Measures.installs.value,
+                enum.Measures.crashes.value,
+            ]
         return self
-    
+
     def sources(self, config):
+        # config で measures と dimension が主に入ってくるのかな？
         self._end_point = "/data/sources/list"
-        for key in ["limit", "group", "dimensionFilters"]:
-            if self.config.get(key):
-                del self.config[key]
+        self._clean_config(["limit", "group", "dimensionFilters"])
         if not self.config.get("limit"):
             self.config["limit"] = 200
         if not self.config.get("dimension"):
@@ -44,21 +50,26 @@ class Query(object):
         self.config.update(config)
         return self
     
+    def limit(self, limit=200):
+        self.config["limit"] = limit
+
+    def _validate_date(self, start, end):
+        try:
+            datetime.strptime(start, "%Y-%m-%d")
+            if end:
+                datetime.strptime(end, "%Y-%m-%d")
+        except ValueError:
+            raise ValueError("Incorrect format, shoube be YYYY-MM-DD.")
 
     def date(self, start, end=None):
-        self.config["start"] = start
-        self.config["end"] = end
+        # TODO: Support datetime
+        self._validate_date(start, end)
+        self.config["start"] = start + "T00:00:000Z"
+        self.config["end"] = end + "T00:00:000Z"
         return self
-        
 
     def assemble_body(self):
-        body = {
-            "startTime": str(self.config["start"]),
-            "endTime": str(self.config["end"]),
-            "adamId": [self.app_id]
-        }
-        body.update(self.config)
-        del body["start"]
-        del body["end"]
-        return body
-
+        self.config["adamId"] = [self.app_id]
+        self.config["startTime"] = self.config.pop("start")
+        self.config["endTime"] = self.config.pop("end")
+        return self.config
